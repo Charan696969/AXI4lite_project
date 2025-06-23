d588524e08a50e173ab6826af7c00cac0ba59439
Charan696969 <charankarthick2310596@ssn.edu.in>
2025-06-23 12:15:53 +0530

    Update interface.sv

--- UVM/interface.sv
+++ UVM/interface.sv
@@ -1,9 +1,9 @@
-interface axi4lite_interface(input logic aclk);
-
-  logic[12:0] aw,ar;
-  logic[35:0] w;
-  logic awvalid, wvalid, bready, arvalid, rready, aresetn, awready, wready, bvalid, arready, rvalid;
-  logic[1:0] b;
-  logic[33:0] r;
-  
-endinterface: axi4lite_interface
+//defining interface for usage
+interface axi4lite_interface(input logic ACLK, input logic ARESETN);
+  logic [12:0] AW, AR;
+  logic [35:0] W;
+  logic AWVALID, WVALID, BREADY, ARVALID, RREADY;
+  logic AWREADY, WREADY, BVALID, ARREADY, RVALID;
+  logic [1:0]  B;
+  logic [33:0] R;
+endinterface
