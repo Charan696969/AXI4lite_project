10a0da64bfb86d0cb73a39c8a4cefbcd97ee15d2
Charan696969 <charankarthick2310596@ssn.edu.in>
2025-06-23 12:14:33 +0530

    Update sequence_item.sv

--- UVM/sequence_item.sv
+++ UVM/sequence_item.sv
@@ -1,36 +1,20 @@
-//Object class - sequence_item
+//object class - sequence_item
+class axi4lite_seq_item extends uvm_sequence_item;
+  //register to factory
+  `uvm_object_utils(axi4lite_seq_item)
 
-class axi4lite_sequence_item extends uvm_sequence_item;
-  `uvm_object_utils(axi4lite_sequence_item)
-  
-  //Instantiations
-  
-  rand logic[12:0] aw,ar;
-  rand logic[35:0] w;
-  rand logic awvalid, wvalid, bready, arvalid, rready, aresetn;
-  logic awready, wready, bvalid, arready, rvalid;
-  logic[1:0] b;
-  logic[33:0] r;
-  
-  constraint address_aligned{
-    aw[1:0] == 2'b00;
-  }
-  
-  constraint strb_valid{
-    if(wvalid) begin
-      w[35:32] != 4'b0000;
-    end
-    else begin
-      w[35:32] == 4'b0000;
-    end
-  }
-  
-  constraint address_range{
-    aw[9:0] inside {[0:1023]};
-  }
-  
-  function new(string name = "axi4lite_sequence_item");
+  //randomisable variables
+  rand bit is_write;
+  rand logic [9:0] addr;
+  rand logic [2:0] prot;
+  rand logic [31:0] wdata;
+  rand logic [3:0] wstrb;
+
+  logic [31:0] rdata;
+  logic [1:0]  resp;
+
+  //constructor
+  function new(string name = "axi4lite_seq_item");
     super.new(name);
-  endfunction: new
-  
-endclass: axi4lite_sequence_item
+  endfunction
+endclass
