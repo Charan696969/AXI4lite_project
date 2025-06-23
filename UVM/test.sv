34cb5d1ac6e8d50d19510a42f315218630ee26e0
Charan696969 <charankarthick2310596@ssn.edu.in>
2025-06-23 12:14:02 +0530

    Update test.sv

--- UVM/test.sv
+++ UVM/test.sv
@@ -1,31 +1,43 @@
+//component class - test
 class axi4lite_test extends uvm_test;
+  //register to factory
   `uvm_component_utils(axi4lite_test)
-  
+  //instantiation
   axi4lite_env env;
+  axi4lite_sequence seq;
   
-  function new(string name = "axi4lite_test", uvm_component parent);
+  //constructor
+  function new(string name, uvm_component parent);
     super.new(name, parent);
-    `uvm_info("TEST_CLASS","Inside axi4lite_test constructor!", UVM_HIGH)
-  endfunction: new
-  
+    `uvm_info("TEST_CLASS","Constructor called!", UVM_MEDIUM)
+  endfunction
+
+  //build phase
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    `uvm_info("TEST_CLASS","Inside axi4lite_test build_phase!", UVM_HIGH)
-    
-    env = axi4lite_env::type_id::create("env",this);
-    
-  endfunction: build_phase
-  
+    env = axi4lite_env::type_id::create("env", this);
+    seq = axi4lite_sequence::type_id::create("seq", this);
+    `uvm_info("TEST_CLASS","Build Phase called!", UVM_MEDIUM)
+  endfunction
+
+  //connect phase
   function void connect_phase(uvm_phase phase);
-     super.connect_phase(phase);
-    `uvm_info("TEST_CLASS","Inside axi4lite_test connect_phase!", UVM_HIGH)
-  endfunction: connect_phase
+    super.connect_phase(phase);
+    `uvm_info("TEST_CLASS","COnnect Phase called!", UVM_MEDIUM)
+  endfunction
+ 
+  //end of elaboration phase
+  virtual function void end_of_elaboration();
+    `uvm_info("TEST_CLASS","Elob Phase!",UVM_MEDIUM)
+    print();
+  endfunction
   
+  //run phase
   task run_phase(uvm_phase phase);
-    super.run_phase(phase);
-    
-    //Fill with logic later
-    
-  endtask: run_phase
+    `uvm_info("TEST_CLASS","Run Phase!",UVM_MEDIUM)
+    phase.raise_objection(this);
+    seq.start(env.agent.seqr);
+    phase.drop_objection(this);
+  endtask
   
-endclass: axi4lite_test
+endclass
